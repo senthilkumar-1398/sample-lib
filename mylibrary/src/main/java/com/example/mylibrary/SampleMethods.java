1a3ac6936d8bfa864b8b509f27b4688d77dc02d2
senthilkumar <senthilkumar@sparkouttech.com>
2023-04-26 16:15:26 +0530

    Sample android lib added

--- mylibrary/src/main/java/com/example/mylibrary/SampleMethods.java
+++ mylibrary/src/main/java/com/example/mylibrary/SampleMethods.java
@@ -1,10 +1,16 @@
 package com.example.mylibrary;
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 public class SampleMethods {
 
     public void showLog() {
         Log.e( "Tag", "showLog" );
+    }
+
+    public void showToast(Context context,String msg) {
+        Toast.makeText( context, msg, Toast.LENGTH_SHORT ).show();
     }
 }
